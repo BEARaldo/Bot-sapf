00015bca94f60036d21d148f6f79125d74d254c9
Geraldo Pereira <95887800+BEARaldo@users.noreply.github.com>
2024-04-03 05:31:16 -0300

    Update cpfAPI_connect.py
    Atualizado para utilizar classe.

--- cpfAPI_connect.py
+++ cpfAPI_connect.py
@@ -1,45 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 
-def capturar_dados(html_response):
-    soup = BeautifulSoup(html_response, 'html.parser')
-    dados_basicos = {}
-    tabela_dados_basicos = soup.find('table', class_='table-sm')
-    if tabela_dados_basicos:
-        linhas = tabela_dados_basicos.find_all('tr')
-        for linha in linhas:
-            rotulo = linha.find('th').text.strip()
-            dado = linha.find('td').text.strip()
-            dados_basicos[rotulo] = dado
-    return dados_basicos
+class cpf_apiSession:
+    def __init__(self):
+        self.base_url = 'https://deskdata.com.br/pessoas/'
+        self.cookie_manual = {
+            'wordpress_logged_in_dd8782f26a34dfd3c646a09580a7757b': 'thihft%40gmail.com%7C1713780026%7Cndq1j1f1cltUZQcJsBU295xW2G3MOxUO1U0Eolk9FWu%7C139d26a84406f74f1e4c4e3ea50efe95880c829c8f9f427a681bb636784ccd8a'
+        } # colocar em json ou variavel de sistema
 
-def api_consulta(cpf):
-    # Configuração inicial, URL e cookies
-    cookie_manual = {
-        'wordpress_logged_in_dd8782f26a34dfd3c646a09580a7757b': 'thihft%40gmail.com%7C1713780026%7Cndq1j1f1cltUZQcJsBU295xW2G3MOxUO1U0Eolk9FWu%7C139d26a84406f74f1e4c4e3ea50efe95880c829c8f9f427a681bb636784ccd8a'
-    }
-    url = "https://deskdata.com.br/pessoas/"
-    payload_cpf = {
-        "key-select": "cpf",
-        "cpf": cpf,
-        "key-extra-select": "",
-        "basic_data": "basic_data",
-        "search_type": "people",
-        "submit": "Consultar",
-        "duplicate-query-confirm": "false"
-    }
-    # Faz a requisição POST
-    response_post = requests.post(url, data=payload_cpf, cookies=cookie_manual, allow_redirects=False)
+    def capturar_dados(self, html_response):
+        soup = BeautifulSoup(html_response, 'html.parser')
+        dados_basicos = {}
+        tabela_dados_basicos = soup.find('table', class_='table-sm')
+        if tabela_dados_basicos:
+            linhas = tabela_dados_basicos.find_all('tr')
+            for linha in linhas:
+                rotulo = linha.find('th').text.strip()
+                dado = linha.find('td').text.strip()
+                dados_basicos[rotulo] = dado
+        return dados_basicos
 
-    # Verifica se houve redirecionamento
-    if response_post.status_code == 302:
-        redirect_url = response_post.headers['Location']
-        response_get = requests.get(redirect_url, cookies=cookie_manual)
-        # Usa a função capturar_dados para extrair as informações do HTML
-        dados = capturar_dados(response_get.text)
-        print(dados)  # Imprime os dados extraídos
-    else:
-        print("Solicitação falhou. Código de status:", response_post.status_code)
+    def consultar_cpf(self, cpf):
+        payload_cpf = {
+            "key-select": "cpf",
+            "cpf": cpf,
+            "key-extra-select": "",
+            "basic_data": "basic_data",
+            "search_type": "people",
+            "submit": "Consultar",
+            "duplicate-query-confirm": "false"
+        }
+        response_post = requests.post(self.base_url, data=payload_cpf, cookies=self.cookie_manual)
 
-cpf = ''#insira o CPF
-api_consulta(cpf)
+        if 'erros' not in response_post.text:
+            dados = self.capturar_dados(response_post.text)
+            return dados
+        else:
+            return "Solicitação falhou. O CPF informado é invalido."
+
+if __name__ == '__main__':
+    client = cpf_apiSession()
+    print(client.consultar_cpf(input("Digite o CPF:")))
