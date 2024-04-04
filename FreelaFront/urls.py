e7bdebac432c85f8707ba39f414f84be58c3f4e4
Dauto Saldanha <dautosaldanhaalves28@gmail.com>
2024-04-04 00:53:09 -0300

    Ta

--- FreelaFront/urls.py
+++ FreelaFront/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for FreelaFront project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from FreelaFront import views, settings
@@ -24,7 +8,12 @@ urlpatterns = [
     path('login2/', views.login2, name="login2"),
     path ('', views.home, name="home"),
     path('pagina1/', views.pagina1, name="pagina1"),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    #testes abaixo:
+    path('choice/', views.choice, name='choice'),
+    path('test/', views.test, name='test'),
+
+    path('reg/', views.reg, name='reg')
 
 
 ]
--- FreelaFront/views.py
+++ FreelaFront/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-
-
 def home(requests):
     return render(requests, 'core/home.html')
 
+#Verifica se o usuário está autenticado antes de chamar a função login2
 @login_required
 def login2(requests):
     return render(requests, 'registration/login.html')
 
+#Verifica se o usuário está autenticado antes de chamar a função pagina1
 @login_required
 def pagina1(requests):
     return render(requests, 'core/pagina1.html')
 
+#Verifica se o usuário está autenticado antes de chamar a função pagina1
+@login_required
+def choice(requests):
+    if requests.method == 'POST':
+        opcao = requests.POST.get('opcao', None)
+        if opcao == 'cpf':
+            return render(requests, 'base/base.html') 
+        elif opcao == 'nome':
+            return render(requests, 'area/nome.html') 
+    return render(requests, 'area/choice.html')
+
+#Verifica se o usuário está autenticado antes de chamar a função pagina1
+@login_required
+def test(requests):
+    return render(requests, 'base/base.html')
 
 
-
+def reg(requests):
+    return render(requests, 'registration/login.html')
