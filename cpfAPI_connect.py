c85e209de13a15f9d9c283d5a2e422e4c9776888
Geraldo Pereira <95887800+BEARaldo@users.noreply.github.com>
2024-04-21 01:38:13 -0300

    Atualização do backend junto ao dos services

--- cpfAPI_connect.py
+++ cpfAPI_connect.py
@@ -7,17 +7,20 @@ class cpf_apiSession:
         self.cookie_manual = {
             'wordpress_logged_in_dd8782f26a34dfd3c646a09580a7757b': 'thihft%40gmail.com%7C1713780026%7Cndq1j1f1cltUZQcJsBU295xW2G3MOxUO1U0Eolk9FWu%7C139d26a84406f74f1e4c4e3ea50efe95880c829c8f9f427a681bb636784ccd8a'
         } # colocar em json ou variavel de sistema
+        self.dados = None
 
     def capturar_dados(self, html_response):
-        soup = BeautifulSoup(html_response, 'html.parser')
+        # soup = BeautifulSoup(html_response, 'html.parser')
         dados_basicos = {}
-        tabela_dados_basicos = soup.find('table', class_='table-sm')
-        if tabela_dados_basicos:
-            linhas = tabela_dados_basicos.find_all('tr')
-            for linha in linhas:
-                rotulo = linha.find('th').text.strip()
-                dado = linha.find('td').text.strip()
-                dados_basicos[rotulo] = dado
+        # tabela_dados_basicos = soup.find('table', class_='table-sm')
+        # if tabela_dados_basicos:
+        #     linhas = tabela_dados_basicos.find_all('tr')
+        #     for linha in linhas:
+        #         rotulo = linha.find('th').text.strip()
+        #         dado = linha.find('td').text.strip()
+        #         dados_basicos[rotulo] = dado
+        dados_basicos ={'nome': 'Geraldo Pereira De Castro Junior', 'dataNascimento': '02/07/1997', 'nomeMae': 'Cleonice Maria De Castro', 'cpf': '056.900.451-95'}
+
         return dados_basicos
 
     def consultar_cpf(self, cpf):
@@ -30,14 +33,15 @@ class cpf_apiSession:
             "submit": "Consultar",
             "duplicate-query-confirm": "false"
         }
-        response_post = requests.post(self.base_url, data=payload_cpf, cookies=self.cookie_manual)
+        # response_post = requests.post(self.base_url, data=payload_cpf, cookies=self.cookie_manual)
 
-        if 'erros' not in response_post.text:
-            dados = self.capturar_dados(response_post.text)
-            return dados
-        else:
-            return "Solicitação falhou. O CPF informado é invalido."
+        # if 'erros' not in response_post.text:
+        #     dados = self.dados(response_post.text)
+        self.dados = {'nome': 'Geraldo Pereira De Castro Junior', 'dataNascimento': '02/07/1997', 'nomeMae': 'Cleonice Maria De Castro', 'cpf': '056.900.451-95'}
+        return self.dados
+        # else:
+        #     return "Solicitação falhou. O CPF informado é invalido."
 
-if __name__ == '__main__':
-    client = cpf_apiSession()
-    print(client.consultar_cpf(input("Digite o CPF:")))
+# if __name__ == '__main__':
+#     client = cpf_apiSession()
+#     print(client.consultar_cpf(input("Digite o CPF:")))
--- sapf_connect.py
+++ sapf_connect.py
@@ -1,15 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-class user_session:
+
+class UserSession:
     def __init__(self):
-        self.login_url = login_url = 'https://sapf.tse.jus.br/sapf/login'
+        self.login_url = 'https://sapf.tse.jus.br/sapf/login'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'
         }
         self.session = requests.Session()
+        self.user_data = None
 
     def login_user(self, username, password):
+        print(username, password)
         response = self.session.get(self.login_url, headers=self.headers)
         soup = BeautifulSoup(response.text, 'html.parser')
         view_state = soup.find('input', {'name': 'javax.faces.ViewState', 'id': 'j_id1:javax.faces.ViewState:0'})['value']
@@ -21,26 +24,25 @@ class user_session:
             'entrar': 'Entrar',
             'javax.faces.ViewState': view_state
         }
-        response = self.session.post(self.login_url, data=payload, headers=self.headers) #realiza o logon
+        response = self.session.post(self.login_url, data=payload, headers=self.headers)
 
-
-        if ("severity:'error'" and "severityText:'Error'") not in response.text:
+        if "severity:'error'" not in response.text and "severityText:'Error'" not in response.text:
             soup = BeautifulSoup(response.text, 'html.parser')
             try:
                 nome_completo = soup.find('p', {'class': 'menu-informacao-texto'}).get_text(strip=True)
                 titulo_eleitoral = soup.find_all('p', {'class': 'menu-informacao-texto'})[1].get_text(strip=True)
                 partido = soup.find_all('p', {'class': 'menu-informacao-texto'})[3].get_text(strip=True)
 
-                self.user_data = {  # Dados do usuario logado
+                self.user_data = {  # Dados do usuário logado
                     'name': nome_completo,
                     'titulo': titulo_eleitoral,
                     'partido': partido
                 }
+                return True
             except Exception as e:
                 print(f"{'-'* 10}\nOcorreu erro:{e}\n{'-'* 10}")
-            return True
+                return False
         else:
-            print("Login/Senha incorreta")
             return False
 
     def access_page(self, page_url):
@@ -51,16 +53,16 @@ class user_session:
             return None
 
 # Teste da classe
-if __name__ == '__main__':
-    ##FUNCIONAL. Adaptar classe e funcionalidades em serviço que pode ser chamado dentro das views do Django
-    user = user_session()
-    # password = getpass.getpass(prompt='Password: ') #usando getpass #import getpass
-    if user.login_user(input("Login:\n"), input("senha:\n")):
-        # Teste da captura dos ddados de user
-        for i in user.user_data:
-            print(i)
-        for key, value in user.user_data.items():
-            print(f"{key}: {value}")
-    else:
-        print("Não foi possível fazer login")
-        #fazer recursividade
+# if __name__ == '__main__':
+#     ##FUNCIONAL. Adaptar classe e funcionalidades em serviço que pode ser chamado dentro das views do Django
+#     user = UserSession()
+#     # password = getpass.getpass(prompt='Password: ') #usando getpass #import getpass
+#     if user.login_user(input("Login:\n"), input("senha:\n")):
+#         # Teste da captura dos dados de user
+#         for i in user.user_data:
+#             print(i)
+#         for key, value in user.user_data.items():
+#             print(f"{key}: {value}")
+#     else:
+#         print("Não foi possível fazer login")
+#         #fazer recursividade
