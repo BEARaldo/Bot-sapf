404c662616ea6bb0cb195bb9886cfb8833d9deef
Dauto Saldanha <dautosaldanhaalves28@gmail.com>
2024-05-01 05:17:20 -0300

    login apenas com o banco de dados

--- FreelaFront/views.py
+++ FreelaFront/views.py
@@ -1,30 +1,20 @@
 import datetime
-from django.shortcuts import render, redirect, reverse
-from django.views.generic import FormView
-from .services.sapf_connect import UserSession
-from .services.tituloEleitoral_connect import ConsultaTituloEleitoral
-from django.http import HttpResponseBadRequest, HttpResponseRedirect, HttpResponse
-from django.contrib.auth import logout
-from django.views.generic import TemplateView, RedirectView
+import os
+from django.shortcuts import render, redirect
+from django.views.generic import FormView, View
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+from django.contrib.auth import logout, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.conf import settings
-import os
 from .forms import LoginForm
-from .services import cpfAPI_connect
-from .services import  generatePdf
-from django.views import View
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.http import HttpResponseNotFound
+from django.urls import reverse
 
 
+from .services import cpfAPI_connect, generatePdf, tituloEleitoral_connect
 
 
-
-
-
-class LoginView(FormView):
+class LoginView(FormView, View):
     template_name = 'login.html'
     form_class = LoginForm
     success_url = '/consultar_cpf/'  # Redirecionar para a URL desejada após o sucesso
@@ -32,42 +22,23 @@ class LoginView(FormView):
     def form_valid(self, form):
         titulo_eleitor = form.cleaned_data['titulo_eleitor']
         password = form.cleaned_data['password']
-        user_session = UserSession()
-        if user_session.login_user(titulo_eleitor, password):
-            self.request.session['user_data'] = user_session.user_data
-            return super().form_valid(form)
+        usuario = authenticate(username=titulo_eleitor, password=password)
+
+        if usuario is not None:
+            return redirect('consultar_cpf')
         else:
-            self.request.session['user_data'] = user_session.user_data
-            return super().form_valid(form)
-            # form.add_error(None, 'Seu título de eleitor ou senha está incorreto.')
-            # return self.form_invalid(form)
-
-    def valida(self):
-        if self.request.method == "GET":
-            return render(self.request, 'login.html')
-        else:
-            titulo_eleitor = self.request.POST.get('titulo_eleitor')
-            senha = self.request.POST.get('password')
-
-            usuario = authenticate(username=titulo_eleitor, password=senha)
+            return redirect('login_view')
 
 
-
-    def form_invalid(self, form):
-        return super().form_invalid(form)
-
-class LogoutView(LoginRequiredMixin, RedirectView):
-    url = reverse_lazy('login_view')
+class LogoutView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         logout(request)
-        return super().get(request, *args, **kwargs)
+        return redirect(reverse_lazy('login_view'))
 
 
 class ConsultaEleitoralView(View):
     def get(self, request):
-        # Recupera variáveis da URL
         consulta_dados = request.session.get('consulta_dados', {})
-        print(f"Eleitoral: {consulta_dados}")
         nome = consulta_dados.get('Nome')
         nomeMae = consulta_dados.get('Nome da mãe')
         dataNascimento = consulta_dados.get('Nascimento')
@@ -75,58 +46,44 @@ class ConsultaEleitoralView(View):
         if not all([nome, nomeMae, dataNascimento]):
             return HttpResponseBadRequest("Todos os parâmetros (nome, cpf, título eleitoral) são obrigatórios.")
 
-        consultor = ConsultaTituloEleitoral("VDRfLIYiiFqEy39v9fr6Q6c-1x4qyUTxzyhVdiIk")
-        self.dados_eleitorais = consultor.execute(dataNascimento, nomeMae, nome)
-        dados_logados = request.session.get('user_data',{})
-        print(dados_logados)
-        self.resultado = {'nome': nome,
-                     'cpf': consulta_dados.get('cpf'),
-                     'nTitulo': self.dados_eleitorais ['nTitulo'],
-                     'zona': self.dados_eleitorais ['zona']}
+        consultor = tituloEleitoral_connect.ConsultaTituloEleitoral("VDRfLIYiiFqEy39v9fr6Q6c-1x4qyUTxzyhVdiIk")
+        dados_eleitorais = consultor.execute(dataNascimento, nomeMae, nome)
 
-        pdf_path = self.pdf_generate()
+        resultado = {
+            'nome': nome,
+            'cpf': consulta_dados.get('cpf'),
+            'nTitulo': dados_eleitorais['nTitulo'],
+            'zona': dados_eleitorais['zona']
+        }
+
+        pdf_path = self.pdf_generate(resultado)
         pdf_path = pdf_path.replace(settings.MEDIA_ROOT, settings.MEDIA_URL)
-        # with open(pdf_path, 'rb') as pdf:
-        #     response = HttpResponse(pdf.read(), content_type='application/pdf')
-        #     response['Content-Disposition'] = 'inline; filename="' + os.path.basename(pdf_path) + '"'
-        #     return response
 
-        return render(request, './test_diretorio/pdf.html', {'resultado': f"{self.resultado['nTitulo']}.pdf"})
-    def pdf_load(self, ):
-        pdf_path = os.path.join(settings.MEDIA_ROOT, 'n logado no SAPF')
+        return render(request, './test_diretorio/pdf.html', {'resultado': f"{resultado['nTitulo']}.pdf"})
 
-        if os.path.exists(pdf_path):
-            with open(pdf_path, 'rb') as pdf:
-                response = HttpResponse(pdf.read(), content_type='application/pdf')
-                response['Content-Disposition'] = 'inline; filename="' + os.path.basename(pdf_path) + self.resultado['nTitulo']
-                return response
-        else:
-            return HttpResponseNotFound('The requested PDF was not found in our records.')
-    def pdf_generate(self, ):
+    def pdf_generate(self, resultado):
+        dados = {
+            'nome': resultado['nome'],
+            'data_d': datetime.date.today().day,
+            'data_m': datetime.date.today().month,
+            'data_a': datetime.date.today().year,
+            'titulo': resultado['nTitulo'],
+            'zona': resultado['zona'],
+            'titulo_coletor': 'n logado no SAPF',
+            'nome_coletor': 'nome logado no SAPF'
+        }
 
-
-
-        dados = {'nome': self.resultado['nome'],
-                 'data_d': datetime.date.today().day,
-                 'data_m': datetime.date.today().month,
-                 'data_a': datetime.date.today().year,
-                 'titulo': self.resultado['nTitulo'],
-                 'zona': self.resultado['zona'],
-                 'titulo_coletor': 'n logado no SAPF',
-                 'nome_coletor': 'nome logado no SAPF'}
-        # dados = {'nome': 'Geraldo Pereira De Castro Junior', 'data1': '22', 'data2': '33', 'data3': '4444',
-        #  'titulo': '025239362089', 'zona': 'DF 005'}
-
-        print(f"pdf dados {dados}")
         input_pdf_path = os.path.join(settings.MEDIA_ROOT, '', 'ficha_apoio.pdf')
         output_pdf_path = os.path.join(settings.MEDIA_ROOT, 'pdfs')
         os.makedirs(output_pdf_path, exist_ok=True)
         out_file = generatePdf.fill_form(input_pdf_path, dados, output_pdf_path + f'/{dados["titulo"]}.pdf')
         return out_file
+
+
 class ServePDF(View):
     def get(self, request, filename):
-        pdf_path = os.path.join(settings.MEDIA_ROOT,'pdfs',filename)
-        print(pdf_path)
+        pdf_path = os.path.join(settings.MEDIA_ROOT, 'pdfs', filename)
+
         if os.path.exists(pdf_path):
             with open(pdf_path, 'rb') as pdf:
                 response = HttpResponse(pdf.read(), content_type='application/pdf')
@@ -134,19 +91,16 @@ class ServePDF(View):
                 return response
         else:
             return HttpResponseNotFound('The requested PDF was not found in our records.')
+
+
 class ConsultaCitizenView(View):
     def post(self, request, *args, **kwargs):
         cpf = request.POST.get('cpf', '')
 
         session = cpfAPI_connect.cpf_apiSession()
         request.session['consulta_dados'] = session.consultar_cpf(cpf)
-        # session['consulta_dados'] = dados
 
         return HttpResponseRedirect(reverse('consulta_eleitoral'))
 
     def get(self, request, *args, **kwargs):
         return render(request, 'area/consultar_cpf.html')
-
-
-
-
